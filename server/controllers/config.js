b39208d2b411284a2ba4ab9f8c6eb687590d76ad
James <james@cybersmith.io>
2024-11-01 15:16:03 -0400

    Add more parameters to support flexible configuration

--- server/controllers/config.js
+++ server/controllers/config.js
@@ -3,7 +3,10 @@ require('dotenv').config();
 
 exports.Config = {
     tenantUrl    : process.env.TENANT_URL,
+    issuerUrl    : process.env.ISSUER_URL,
     authnUrl     : process.env.AUTHORIZATION_URL,
+    tokenUrl     : process.env.TOKEN_URL,
+    userInfoUrl  : process.env.USERINFO_URL,
     clientId     : process.env.CLIENT_ID,
     clientSecret : process.env.CLIENT_SECRET,
     redirectUri  : process.env.REDIRECT_URI,
@@ -13,4 +16,6 @@ exports.Config = {
     serverHttps  : process.env.SERVER_HTTPS,
     serverPort   : process.env.SERVER_PORT,
     fqdn         : process.env.FQDN,
+    debug        : process.env.DEBUG,
+    rejectUnauthorized : process.env.REJECT_UNAUTHORIZED,
 };
